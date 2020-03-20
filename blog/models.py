e0ddd03f00d78dd8c2a2c76c0221e3bb554d8ac6
samikhailov <ko_0n@mail.ru>
2020-03-20 13:54:45 +0300

    Поддержан markdown для входящего текста

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+import markdown2
 
 
 class Post(models.Model):
@@ -12,6 +13,9 @@ class Post(models.Model):
 
     def reading_duration(self):
         return len(self.text) // 1500 + 1
+
+    def html_text(self):
+        return markdown2.markdown(self.text, extras=["fenced code blocks"])
 
     def __str__(self):
         return self.title
